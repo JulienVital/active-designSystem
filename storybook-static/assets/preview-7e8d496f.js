9653df65282039f116855f796e4e77951c82e3c6
Github action <githubAction@email.com>
2023-10-30 14:12:21 +0000

    Deploy Storybook

---
+++ storybook-static/assets/preview-7e8d496f.js
@@ -0,0 +1,2 @@
+const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{e as default};
+//# sourceMappingURL=preview-7e8d496f.js.map
