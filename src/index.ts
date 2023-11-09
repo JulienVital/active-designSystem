33d4525cb35e434ceb492b800a7c535d13be4b17
Julien VITAL <jvital@datasolution.fr>
2023-11-09 15:29:01 +0100

    add export dropzone

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import '@/assets/main.css'
 import InputNumber from '@/components/Inputs/InputNumber/InputNumber.vue'
 import InputText from '@/components/Inputs/InputText/InputText.vue'
 import InputSelect from '@/components/Inputs/InputSelect/InputSelect.vue'
-export { InputNumber, InputText, InputSelect }
+import DropZone from '@/components/Inputs/DropZone/DropZone.vue'
+export { InputNumber, InputText, InputSelect, DropZone }
