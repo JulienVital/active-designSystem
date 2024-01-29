b774b99140bcdf6a9fc0977dc8a51158344e6b30
Julien VITAL <jvital@datasolution.fr>
2024-01-29 16:10:16 +0100

    add .

--- src/components/Panels/Tabview/ApTabView.vue
+++ src/components/Panels/Tabview/ApTabView.vue
@@ -9,12 +9,37 @@ import TabView from 'primevue/tabview'
 </script>
 <style>
 .tabview{
-
-  padding-left: 0;
-  margin: 0;
+  padding: 0 var(--gutter);
 }
-.tabview li +li {
+.tabview ul {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
   margin: 0;
+  padding: 0;
+  margin: 20px;
+}
+.tabview li  + li{
+  margin: 0;
+  margin-right: 15px;
 
+}
+.tabview li {
+  margin-right: 15px;
+}
+.tabview .p-tabview-nav-link{
+  color: var(--color-text-secondary);
+  padding-bottom: 4px;
+  text-decoration: none;
+  font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    font-family: Poppins;
+}
+
+.tabview .p-highlight .p-tabview-nav-link, .tabview .p-tabview-nav-link:hover {
+  color: var(--color-text);
+  border-bottom: 1px solid var(--color-text);
 }
 </style>
